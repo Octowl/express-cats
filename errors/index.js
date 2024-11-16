df6dbae221cadc811ce8dd1e96af5323a9931b63
Octowl <alsaff1987@gmail.com>
2024-11-17 02:57:10 +0300

    custom validation error

--- errors/index.js
+++ errors/index.js
@@ -12,4 +12,20 @@ function BadRequestError(message, status = 400) {
   return CustomError({ message, status });
 }
 
-module.exports = { NotFoundError, BadRequestError };
+function RequestValidationError(
+  errors,
+  message = "Invalid request parameters",
+  status = 400
+) {
+  const err = CustomError({ message, status });
+
+  err.response = errors.map((error) => {
+    if (error.type === "field") {
+      return { message: error.msg, field: error.path };
+    }
+    return { message: error.msg };
+  });
+  return err;
+}
+
+module.exports = { NotFoundError, BadRequestError, RequestValidationError };
