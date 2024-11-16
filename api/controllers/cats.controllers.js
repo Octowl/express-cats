0bff20917d6073bec0d8922dc5065fa743879e76
Octowl <alsaff1987@gmail.com>
2024-11-17 02:27:26 +0300

    drop try catch

--- api/controllers/cats.controllers.js
+++ api/controllers/cats.controllers.js
@@ -1,53 +1,38 @@
 const Cat = require("../models/Cat");
 
-const catList = async function (req, res, next) {
+const catList = async function (req, res) {
   const { name } = req.query;
 
-  try {
-    const cats = await Cat.find(
-      { name: { $regex: name ?? "", $options: "i" } },
-      "-__v"
-    );
-    res.status(200).json(cats);
-  } catch (error) {
-    next(error);
-  }
+  const cats = await Cat.find(
+    { name: { $regex: name ?? "", $options: "i" } },
+    "-__v"
+  );
+  res.status(200).json(cats);
 };
 
 const catDetail = async function (req, res) {
   res.status(200).json(req.cat);
 };
 
-const createCat = async function (req, res, next) {
-  try {
-    if (req.file)
-      req.body.image = `${req.protocol}://${req.get("host")}/${req.file.path}`;
-    const newCat = await Cat.create(req.body);
-    res.status(201).json(newCat);
-  } catch (error) {
-    next(error);
-  }
+const createCat = async function (req, res) {
+  if (req.file)
+    req.body.image = `${req.protocol}://${req.get("host")}/${req.file.path}`;
+  const newCat = await Cat.create(req.body);
+  res.status(201).json(newCat);
 };
 
 const updateCat = async function (req, res, next) {
-  try {
-    if (req.file)
-      req.body.image = `${req.protocol}://${req.get("host")}/${req.file.path}`;
+  if (req.file)
+    req.body.image = `${req.protocol}://${req.get("host")}/${req.file.path}`;
 
-    for (const key in req.body) req.cat[key] = req.body[key];
-    req.cat.save();
-    res.status(200).json(req.cat);
-  } catch (error) {
-    next(error);
-  }
+  for (const key in req.body) req.cat[key] = req.body[key];
+
+  req.cat.save();
+  res.status(200).json(req.cat);
 };
 
 const deleteCat = async function (req, res, next) {
-  try {
-    await req.cat.deleteOne();
-  } catch (error) {
-    next(error);
-  }
+  await req.cat.deleteOne();
   res.status(204).end();
 };
 
