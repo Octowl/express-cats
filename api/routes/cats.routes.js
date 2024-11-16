1887569b2d7fe024c137ad7180788972c806840e
Octowl <alsaff1987@gmail.com>
2024-11-17 02:57:47 +0300

    use validation middleware

--- api/routes/cats.routes.js
+++ api/routes/cats.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body } = require("express-validator");
 
 const {
   catList,
@@ -7,7 +8,12 @@ const {
   updateCat,
   deleteCat,
 } = require("../controllers/cats.controllers");
-const { findCat, rejectDemonCats, upload } = require("../../middleware");
+const {
+  findCat,
+  rejectDemonCats,
+  upload,
+  validateRequest,
+} = require("../../middleware");
 
 const router = express.Router();
 
@@ -15,8 +21,34 @@ router.param("catSlug", findCat);
 
 router.get("/", catList);
 router.get("/:catSlug", catDetail);
-router.post("/", upload.single("image"), rejectDemonCats, createCat);
-router.put("/:catSlug", upload.single("image"), updateCat);
+
+router.post(
+  "/",
+  upload.single("image"),
+  rejectDemonCats,
+  [
+    body("name").not().isEmpty().withMessage("Name is required"),
+    body("age")
+      .isInt({ gt: -1 })
+      .withMessage("Age must be a number greater than or equal to 0"),
+  ],
+  validateRequest,
+  createCat
+);
+
+router.put(
+  "/:catSlug",
+  upload.single("image"),
+  [
+    body("name").not().isEmpty().withMessage("Name is required"),
+    body("age")
+      .isInt({ gt: -1 })
+      .withMessage("Age must be a number greater than or equal to 0"),
+  ],
+  validateRequest,
+  updateCat
+);
+
 router.delete("/:catSlug", deleteCat);
 
 module.exports = router;
