0d7474580d14b05adcbd56166e5cff2c3cc857fa
Octowl <alsaff1987@gmail.com>
2024-11-17 02:57:36 +0300

    improved error handling

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,17 +1,10 @@
 const handleErrors = function (err, req, res, next) {
-  if (err.name === "ValidationError") {
-    let errors = {};
-
-    for (const key in err.errors) {
-      errors[key] = err.errors[key].message;
-    }
-
-    return res.status(400).json(errors);
-  }
-
-  res
-    .status(err.status ?? 500)
-    .json({ message: err.message ?? "Internal Server Error" });
+  console.error(err);
+  res.status(err.status ?? 500).json({
+    errors: err.response ?? [
+      { message: err.message ?? "Internal Server Error" },
+    ],
+  });
 };
 
 module.exports = handleErrors;
